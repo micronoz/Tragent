3f0450cc729c74e112b4c82e9601fe80aa10f478
Nabi Can Ozberkman <nozberkm@ucsd.edu>
2019-01-03 16:21:34 -0800

    Needs organizing

--- model.py
+++ model.py
@@ -3,46 +3,109 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 import math
+import numpy as np
 from collections import OrderedDict
+
+
+def conv3(in_channels, out_channels, stride=1):
+    """ 3x1 convolutional layer for Bottleneck """
+    return nn.Conv2d(in_channels, out_channels, kernel_size=(3,1), padding=(1,0), bias=False, stride=stride)
+
+def conv1(in_channels, out_channels, stride=1):
+    """ 1x1 convolutional layer for Bottleneck """
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False, stride=stride)
 
 class Bottleneck(nn.Module):
     expansion = 4
-    def __init__(self, in_channels, out_channels, stride, cardinality, base_width):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         """
         Implementation of the Bottleneck block from the ResNeXt architecture.
 
-        in_channels: size of input channels
-        out_channels: size of output channels
-        stride: stride for the convolutional layers
-        cardinality: cardinality as described in the paper
-        base_width:
+        Args:
+            in_channels: size of input channels
+            out_channels: size of output channels
+            stride: stride for the convolutional layers
+            downsample: method for increasing channels width of input
         """
         super(Bottleneck, self).__init__()
-        D = int(math.floor(out_channels * (base_width / 64.)))
-        C = cardinality
+        self.conv1 = conv1(in_channels, out_channels)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = conv3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = conv1(out_channels, out_channels * self.expansion)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
 
-        self.conv_reduce = nn.Conv2d(in_channels, D*C, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn_reduce = nn.BatchNorm2d(D*C)
+    def forward(self, x):
+        identity = x
 
-        self.conv_group = nn.Conv2d(D*C, D*C, kernel_size=(3,1), stride=1, padding=(1,0), groups=C, bias=False)
-        self.bn_group = nn.BatchNorm2d(D*C)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+ 
+        if self.downsample is not None:
+            identity = self.downsample(x)
 
-        self.conv_expand = nn.Conv2d(D*C, out_channels, kernel_size=1, stride=1, bias=False)
-        self.bn_expand = nn.BatchNorm2d(out_channels)
-
-    def forward(self,x):
-        residual = x
-
-        out = self.conv_reduce(x)
-        out = F.relu(self.bn_reduce(x), inplace=True)
-
-        out = self.conv_group(x)
-        out = F.relu(self.bn_group(x), inplace=True)
-        
-        out = self.conv_expand(x)
-        out = F.relu(self.bn_expand(x) + residual, inplace=True)
-
+        out += identity
+        out = self.relu(out)
         return out
+
+class ResNet(nn.Module):
+    def __init__(self, layers, num_classes, block=Bottleneck):
+        super(ResNet, self).__init__()
+        self.init_planes = 64
+        self.num_classes = num_classes
+        self.init_conv = nn.Conv2d(3, self.init_planes, kernel_size=(7,1), 
+                                stride=(1,1), padding=(3,0), bias=False)
+        self.init_bn = nn.BatchNorm2d(self.init_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AvgPool2d(kernel_size=(2,1), stride=(1,1))
+        
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1])
+        self.layer3 = self._make_layer(block, 256, layers[2])
+        self.layer4 = self._make_layer(block, 512, layers[3])
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1,5))
+        self.adaptive_pool_1x1 = nn.AdaptiveAvgPool2d((1,1))
+
+    def _make_layer(self, block, out_channels, blocks):
+        downsample = None
+        if self.init_planes != out_channels * block.expansion:
+            downsample = nn.Sequential(conv1(self.init_planes, out_channels * block.expansion),
+                                        nn.BatchNorm2d(out_channels * block.expansion))
+        layers = []
+        layers.append(block(self.init_planes, out_channels, 1, downsample))
+        self.init_planes = out_channels * block.expansion
+        for _ in range(1,blocks):
+            layers.append(block(self.init_planes, out_channels))
+        
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        result = self.init_conv(x)
+        result = self.init_bn(result)
+        result = self.relu(result)
+        #result = self.avgpool(result)
+        result = self.layer1(result)
+        result = self.avgpool(result)
+        result = self.layer2(result)
+        result = self.layer3(result)
+        result = self.layer4(result)
+        result = self.adaptive_pool(result)
+        result = torch.transpose(result, 1, 3)
+        result = self.adaptive_pool_1x1(result)
+        result = result.view(-1, self.num_classes)
+        result = F.softmax(result, dim=1)
+        return result
+    
+
 
 class _DenseLayer(nn.Sequential):
     def __init__(self, input_features, growth_rate, expansion):
@@ -58,14 +121,15 @@ class _DenseLayer(nn.Sequential):
         self.add_module('norm1', nn.BatchNorm2d(input_features)),
         self.add_module('relu1', nn.ReLU(inplace=True)),
         self.add_module('conv1', nn.Conv2d(input_features, expansion *
-                        growth_rate, kernel_size=1, stride=1, bias=False)),
+                        growth_rate, kernel_size=1, stride=1, bias=True)),
         self.add_module('norm2', nn.BatchNorm2d(expansion * growth_rate)),
         self.add_module('relu2', nn.ReLU(inplace=True)),
         self.add_module('conv2', nn.Conv2d(expansion * growth_rate, growth_rate,
-                        kernel_size=(3,1), stride=1, padding=(1,0), bias=False))
+                        kernel_size=(3,1), stride=1, padding=(1,0), bias=True))
 
     def forward(self, x):
-        return torch.cat([x, super().forward(x)],1)
+        out = super(_DenseLayer, self).forward(x)
+        return torch.cat([out, x],1)
 
 class _DenseBlock(nn.Sequential):
     def __init__(self, num_layers, input_features, growth_rate, expansion):
@@ -82,18 +146,18 @@ class _Transition(nn.Sequential):
         self.add_module('norm', nn.BatchNorm2d(input_features))
         self.add_module('relu', nn.ReLU(inplace=True))
         self.add_module('conv', nn.Conv2d(input_features, output_features,
-                                          kernel_size=1, stride=1, bias=False))
+                                          kernel_size=1, stride=1, bias=True))
         self.add_module('pool', nn.AvgPool2d(kernel_size=(2,1), stride=(2,1)))
 
 class DenseNet(nn.Module):
     def __init__(self, num_classes, growth_rate=32, 
-                layer_config=(6,12,24,48), init_layer=64, expansion=4):
+                layer_config=(6,12,24,16), init_layer=64, expansion=4, reduce=False):
         super(DenseNet, self).__init__()
         self.num_classes = num_classes
 
         #Initial layer
         self.features = nn.Sequential(OrderedDict([
-            ('init_conv', nn.Conv2d(3, init_layer, kernel_size=1, stride=1, bias=False)),
+            ('init_conv', nn.Conv2d(3, init_layer, kernel_size=1, stride=1, bias=True)),
             ('init_bn', nn.BatchNorm2d(init_layer)),
             ('init_relu', nn.ReLU(inplace=True))
         ]))
@@ -101,26 +165,39 @@ class DenseNet(nn.Module):
         #Dense blocks
         channels = init_layer
         for i, layer_count in enumerate(layer_config):
-            block = _DenseBlock(layer_count, channels, growth_rate, expansion)
-            channels += layer_count * expansion
+            block = _DenseBlock(layer_count, channels, growth_rate=growth_rate, expansion=expansion)
+            channels = channels + layer_count * growth_rate
             self.features.add_module('denseblock%d' % (i+1), block)
             
             #Transitions
-            if i != len(layer_config) - 1:
+            if i != len(layer_config) - 1 and reduce:
                 trans = _Transition(channels, channels // 2)
                 self.features.add_module('transition%d' % (i+1), trans)
                 channels = channels // 2
         
         #Decision layer
         self.features.add_module('final_pool', nn.AdaptiveAvgPool2d((1,num_classes)))
-        self.features.add_module('final_conv', nn.Conv2d(channels, num_classes, (1,num_classes), bias=False))
+        self.features.add_module('final_relu', nn.ReLU(inplace=True))
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
         
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(-1,self.num_classes)
-        x = F.softmax(x)
-        return x
-        
+        result = self.features(x)
+        final_conv = nn.Conv2d(result.size(1), 50, kernel_size=(1,1), bias=True)
+        final_conv_2 = nn.Conv2d(50, 1, kernel_size=(1,1), bias=True)
+        result = final_conv(result)
+        result = final_conv_2(result)
+        result = result.view(-1,self.num_classes)
+        result = F.softmax(result, dim=1)
+        return result
+
 # To be implemented
 # class ResNeXt(nn.Module):
 #     def __init__(self, bottleneck, depth, cardinality, base_width, num_classes):
@@ -132,7 +209,7 @@ class DenseNet(nn.Module):
 #         self.num_classes = num_classes
 #         self.expansion = 128
 
-#         self.initial_conv = nn.Conv2d(3,self.expansion,(3,1),1,(1,0),bias=False)
+#         self.initial_conv = nn.Conv2d(3,self.expansion,(3,1),1,(1,0),bias=True)
 #         self.initial_bn = nn.BatchNorm2d(self.expansion)
 
 #         self.in_channels = self.expansion
--- train.py
+++ train.py
@@ -15,6 +15,7 @@ import torch.optim as optim
 import time
 import argparse
 import os
+from model import DenseNet, ResNet
 
 
 # In[2]:
@@ -30,7 +31,34 @@ os.environ["CUDA_VISIBLE_DEVICES"]=str(args.cuda_dev)
 
 
 # In[3]:
-
+def loss_fn(net, batch_in):
+    d,y = batch_in
+    d = d.float()
+    y = y.float()
+    
+    y[y <= 0.0] = 1.0
+    y[y > 3.0] = 1.0
+    saved = y
+    #y = torch.tan(-y - 1)
+    #y = y ** -1
+    y = (y ** -1) ** 2
+#         d = test_X
+#         y = test_y
+    d = d.cuda()
+    x = net(d)
+    y = (y.reshape([-1,currency_count])).cuda()
+#         print(x.shape)
+#         print(y.shape)
+    readable_loss = torch.mul(x,saved.reshape([-1,currency_count]).cuda()).sum(dim=1)
+    s = readable_loss.shape[0]
+    readable_loss = readable_loss.sum()/s
+    z = torch.mul(x,y)
+    z = z.sum(dim=1)
+    loss = z.sum()
+    loss /= z.shape[0]
+    del y
+    del d
+    return loss,readable_loss
 
 class CurrencyDataset(Dataset):
     def __init__(self,root_dir):
@@ -44,83 +72,10 @@ class CurrencyDataset(Dataset):
     def __len__(self):
         return len(os.listdir(os.path.join(self.dir,'Batches')))
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3,512,(1,1))
-        self.conv2 = nn.Conv2d(512,512,(3,1),padding=(1,0))
-        self.conv3 = nn.Conv2d(512,512,(3,1),padding=(1,0))
-        self.conv4 = nn.Conv2d(512,1024,(3,1))
-        self.conv5 = nn.Conv2d(1024,1024,(3,1))
-        self.conv6 = nn.Conv2d(1024,1024,(3,1))
-        self.conv7 = nn.Conv2d(1024,2048,(3,1))
-        self.conv8 = nn.Conv2d(2048,2048,(3,1))
-        self.conv11 = nn.Conv2d(2048,2048,(4,1))
-        self.conv9 = nn.Conv2d(2048,2048,(3,1))
-        self.conv10 = nn.Conv2d(2048,1,(1,1))
-        self.activation = F.leaky_relu
-        self.pool = nn.AvgPool2d((3,1))
-        self.norm_512 = nn.BatchNorm2d(512)
-        self.norm_1024 = nn.BatchNorm2d(1024)
-        self.norm_2048 = nn.BatchNorm2d(2048)
-        self.norm_4096 = nn.BatchNorm2d(4096)
-        self.norm_10000 = nn.BatchNorm2d(10000)
-#         self.conv_test = nn.Conv2d(5,128,(48,3))
-#         self.conv_test2 = nn.Conv2d(128,5,(3,1))
-        self.fc1 = nn.Linear(5, 5)
-        self.dp1 = nn.Dropout(p=0.3)
-        self.fc2 = nn.Linear(3000, 3500)
-        self.fc3 = nn.Linear(3500, 5)
 
-    def forward(self, x):
-     #   x = self.dp1(x)
-        
-        x = self.activation(self.norm_512(self.conv1(x)))
-        res = x
-        x = self.activation(self.norm_512(self.conv2(x)))
-        x += res
-        x = self.activation(self.pool(x))
-#         print(x.shape)
-        x = self.activation(self.norm_512(self.conv3(x)))
-#         print(x.shape)
-        x = self.activation(self.norm_1024(self.conv4(x)))
-#         print(x.shape)
-        x = self.activation(self.norm_1024(self.conv5(x)))
-#         print(x.shape)
-        x = self.activation(self.norm_1024(self.conv6(x)))
-#         print(x.shape)
-        x = self.activation(self.norm_2048(self.conv7(x)))
-#         print(x.shape)
-        x = self.activation(self.norm_2048(self.conv8(x)))
-        x = self.activation(self.norm_2048(self.conv9(x)))
-        x = self.activation(self.norm_2048(self.conv11(x)))
-        x = self.conv10(x)
-#         print(x.shape)
-#         x = F.relu(self.conv_test(x))
-#         x = self.conv_test2(x)
-        
-        print(x.shape)
-#         print(x.shape)
-#         x = x.view(-1,5)
-#         print(x.shape)
-#         x = x.view(-1,5)
-#         print(x.shape)       
-#         x = F.relu(self.fc1(x))
-#         print(x.shape)
-# #         x = self.dp1(x)
-#         x = F.relu(self.fc2(x))
-# #         x = self.dp1(x)
-#         x = self.fc3(x)
-        x = x.view(-1,5)
-#         x = self.dp1(x)
-#         print(x.shape)
-        
-    
-        x = F.softmax(x,dim=1)
-        return x
 def weight_reset(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-        torch.nn.init.xavier_uniform_(m.weight)
+        torch.nn.init.kaiming_normal_(m.weight)
 
 
 # In[4]:
@@ -132,7 +87,9 @@ torch.set_default_tensor_type('torch.cuda.FloatTensor')
 data = CurrencyDataset('./Processed')
 device = 'cuda:0'
 currency_count = 5
-net = Net()
+#net = DenseNet(currency_count,growth_rate=1)
+#net = Net()
+net = ResNet([3,8,36,3], currency_count)
 print(net)
 # net = nn.DataParallel(net,device_ids=[0])
 net.cuda()
@@ -142,17 +99,28 @@ net.cuda()
 #         layer.float()
 # net = net.to(device)
 net.train()
-#dataloader = DataLoader(data,batch_size=1024,pin_memory=True, sampler=SubsetRandomSampler(list(range(1024,2048))))
-dataloader = DataLoader(data,batch_size=512,pin_memory=True,shuffle=True)
+dataloader = DataLoader(data,batch_size=50,pin_memory=True, sampler=SubsetRandomSampler(list(range(5000,15000))))
+test_loader = DataLoader(data,batch_size=5,pin_memory=True, sampler=SubsetRandomSampler(list(range(3400,3500))))
+#dataloader = DataLoader(data,batch_size=50,pin_memory=True,shuffle=True)
 
 
 # In[5]:
 
 
-optimizer = optim.Adam(net.parameters(), lr=0.00015)
+optimizer = optim.Adam(net.parameters(), lr=0.01)
 
 
 # In[6]:
+
+def test(model, test_loader, epoch):
+    model.eval()
+    loss = 0
+    j = 0
+    for i,b in enumerate(test_loader, 0):
+        j = i
+        _, readable_loss = loss_fn(model, b)
+        loss += readable_loss.item() * 10000
+    print('Epoch {} test gain: {}\n'.format(epoch+1, loss/(j+1)))
 
 
 net.apply(weight_reset)
@@ -161,64 +129,29 @@ net.apply(weight_reset)
 # print(test_y)
 print_every = 16
 begin = time.time()
-for epoch in range(100):
+for epoch in range(200):
     epoch_loss = 0.0
     running_loss = 0.0
+    net.train()
     j = 0
     for i,batch in enumerate(dataloader, 0):
         j = i
         optimizer.zero_grad()
-        d,y = batch
-        d = d.float()
-        y = y.float()
-        saved = y
-        y[y <= 0.0] = 1.0
-        y[y > 3.0] = 1.0
-        y = torch.tan(-y - 1)
-        #y = y ** -1
-        #y = (y ** -1) ** 2
-        if np.isnan(saved).any():
-            print('Error')
-            continue
-#         d = test_X
-#         y = test_y
-        d = d.cuda()
-        x = net(d)
-        y = (y.reshape([-1,5])).cuda()
-#         print(x.shape)
-#         print(y.shape)
-        readable_loss = torch.mul(x,saved.reshape([-1,5]).cuda()).sum(dim=1)
-        s = readable_loss.shape[0]
-        readable_loss = readable_loss.sum()/s
-        z = torch.mul(x,y)
-        z = z.sum(dim=1)
-        loss = z.sum()
-        loss /= z.shape[0]
-#         loss = loss ** -1
+        loss, readable_loss = loss_fn(net, batch)
         epoch_loss += readable_loss.item() * 10000
-        running_loss += readable_loss.item() * 10000
         
-        if i % print_every == print_every - 1:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / print_every))
-            print('Time elapsed: {} minutes'.format((time.time() - begin)/60))
-            running_loss = 0.0
+        # if i % print_every == print_every - 1:
+        #     print('[%d, %5d] loss: %.3f' %
+        #           (epoch + 1, i + 1, running_loss / print_every))
+        #     print('Time elapsed: {} minutes'.format((time.time() - begin)/60))
+        #     running_loss = 0.0
 
         loss.backward()
         optimizer.step()
-    print('Epoch {} loss: {}'.format(epoch+1, epoch_loss/(j+1)))
+    print('Epoch {} training gain: {}'.format(epoch+1, epoch_loss/(j+1)))
+    test(net, test_loader, epoch)
 end = time.time()
 print("FINAL TIME: {}".format(end-begin))
 
-
-# In[ ]:
-
-
-# net.eval()
-# for i,batch in enumerate(dataloader):
-#     d,y = batch
-#     print(y)
-#     print(net(d.cuda()))
-#     break
 
 
